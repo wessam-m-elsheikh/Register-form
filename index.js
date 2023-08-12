59e184f0648e28cf1e90e447716a6aaa6cf71077
wessam-m-elsheikh <96388872+wessam-m-elsheikh@users.noreply.github.com>
2023-08-12 11:12:08 +0300

    final edits

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 "use strict";
-
-const form = document.querySelector("#survey-form");
+// to get form data in a well formated object
+const form = document.querySelector("form");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   const data = [];
   const formData = new FormData(form);
   for (const item of formData) {
     data.push(item);
-    // data.push({item[0]: item[1]});
   }
   const userData = Object.fromEntries(data);
   console.log(userData);
+  form.reset();
 });
